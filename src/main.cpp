2e156b377b43340876da29748bde29a3269384dc
Your Name <you@example.com>
2016-01-11 20:26:02 -0800

    setting new coin parameters

--- src/main.cpp
+++ src/main.cpp
@@ -828,13 +828,13 @@ uint256 static GetOrphanRoot(const CBlock* pblock)
 
 int64 static GetBlockValue(int nHeight, int64 nFees)
 {
-    int64 nSubsidy = 1 * COIN;
-
+    int64 nSubsidy = 11 * COIN;
+//11 young coconuts are harvested for each climb (on a good tree)
     return nSubsidy + nFees;
 }
 
 static const int64 nTargetTimespan = 1 * 24 * 60 * 60; // coconuts: 1 days
-static const int64 nTargetSpacing = 120; // coconuts: 2 minute blocks
+static const int64 nTargetSpacing = 420; // coconuts: 8 minute harvests
 static const int64 nInterval = nTargetTimespan / nTargetSpacing;
 
 // Thanks: Balthazar for suggesting the following fix
@@ -2002,7 +2002,7 @@ bool LoadBlockIndex(bool fAllowNew)
     
         
         // Genesis block
-        const char* pszTimestamp = "Building a coin to show the world how...";
+        const char* pszTimestamp = "Coconut tree is no tree? Goa govt is nuts says Oppn - Jan 11th 2016 from The Tribune published in Chandigarh India";
         CTransaction txNew;
         txNew.vin.resize(1);
         txNew.vout.resize(1);
@@ -2014,14 +2014,14 @@ bool LoadBlockIndex(bool fAllowNew)
         block.hashPrevBlock = 0;
         block.hashMerkleRoot = block.BuildMerkleTree();
         block.nVersion = 1;
-        block.nTime    = 1369623856; //epochtime
+        block.nTime    = 1452572845; //epochtime
         block.nBits    = 0x1e0ffff0;
-        block.nNonce   = 1345972;
+        block.nNonce   = 0;
 
         if (fTestNet)
         {
-            block.nTime    = 1369591342;
-            block.nNonce   = 440824;
+            block.nTime    = 1452572845;
+            block.nNonce   = 0;
         }
 
         //// debug print
